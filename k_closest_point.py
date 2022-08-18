e2f0935c2cad0e6ae12d24872458434422cc80e1
Hiwot Derese <Hiwotderese16@gmail.com>
2022-08-18 21:08:53 +0300

    correction for k_closest answer

--- k_closest_point.py
+++ k_closest_point.py
@@ -1,25 +1,14 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        lis = []
         output = []
-        dis_bet = sqrt((points[0][0] ** 2) + (points[0][1] ** 2))
-        x = points[0][0]
-        y = points[0][1]
-        for i in range(len(points)):
-            distance = sqrt((points[i][0] ** 2) + (points[i][1] ** 2))
-            if distance < dis_bet:
-                dis_bet = distance
-                x = points[i][0]
-                y = points[i][1]
-            elif (i > 0): 
-                if distance == dis_bet:
-                    new = []
-                    new.insert(0, points[i][0])
-                    new.insert(1, points[i][1])
-                    lis.append(new)
-        output.insert(0, x)
-        output.insert(1, y)
-        lis.append(output)
-        return lis
-            # print(output)
+        for i in range(len(points) - 1):
+            for j in range(i+1,len(points),1):
+                distance = sqrt((points[i][0] ** 2) + (points[i][1] ** 2))
+                if distance > sqrt((points[j][0] ** 2) + (points[j][1] ** 2)):
+                    points[i], points[j] = points[j], points[i]
+                    
+        for i in range(0,k,+1):
+            output.append(points[i])
+        return output
+               
         
