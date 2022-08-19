56c5a1904423b36dfde86351013ecd0803af2598
Hiwot Derese <Hiwotderese16@gmail.com>
2022-08-19 09:38:18 +0300

    1968_on_leetcode

---
+++ elt_not_equal_average_of_nighbors.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rearrangeArray(self, nums: List[int]) -> List[int]:
+        for i in range(1,len(nums)-1, 1):
+            if i == len(nums):
+                break
+            if (nums[i-1] + nums[i+1]) / 2 == nums[i]:
+                nums[i], nums[i + 1] = nums[i + 1], nums[i]
+            for i in range(i, 1, -1):
+                if (nums[i] + nums[i -2]) / 2 == nums[i -1]:
+                    nums[i-1], nums[i] = nums[i], nums[i-1]
+                else: 
+                    break
+        return nums
