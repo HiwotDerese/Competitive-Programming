b8da0e08fe135851a8ef8ffd852c190ee34eeb65
Hiwot Derese <Hiwotderese16@gmail.com>
2022-09-03 18:27:38 +0300

    969_Leetcode

---
+++ pancake_sorting.py
@@ -0,0 +1,11 @@
+class Solution:
+    def pancakeSort(self, arr: List[int]) -> List[int]:
+        ans = []
+        for i in range(len(arr)):
+            maxVal = max(arr[:len(arr)-i]) 
+            maxIdx = arr.index(maxVal)+1
+            arr[:maxIdx] = reversed(arr[:maxIdx])
+            ans.append(maxIdx)
+            arr[:len(arr)-i] = reversed(arr[:len(arr)-i])
+            ans.append(len(arr)-i)
+        return ans
