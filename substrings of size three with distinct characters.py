fb21ad4b1b6debe197a622161b5d8713bd01da23
Hiwot Derese <Hiwotderese16@gmail.com>
2022-10-03 15:10:43 +0300

    1876Leetcode

--- substrings of size three with distinct characters.py
+++ substrings of size three with distinct characters.py
@@ -1,10 +1,12 @@
-def countGoodSubstrings(self, s: str) -> int:
-        i=0
-        j=2
-        c=0
-        while j<len(s):
-            if len(set(s[i:j+1]))==3:
-                c+=1
-            i+=1
-            j=i+2
-        return c
+class Solution:
+    def countGoodSubstrings(self, s: str) -> int:
+        count = 0
+        k = 3
+        st = s[:k]
+        for i in range(len(s) - 2):
+            if len(set(st)) == 3:
+                count += 1
+            st = s[i+1:k+1]
+            k += 1
+        return count
+        
