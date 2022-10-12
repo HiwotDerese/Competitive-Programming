23f7624d30131cdd0f7aeeff87a74db8c694d114
Hiwot Derese <Hiwotderese16@gmail.com>
2022-10-12 23:46:46 +0300

    1Leetcode

--- Two Sum.py
+++ Two Sum.py
@@ -8,3 +8,26 @@ class Solution:
                     output.append(j)
                     break
         return output
+
+# ********************************************************************************************************
+# two pointers method
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr2 = sorted(nums)
+        l = 0
+        r = len(nums) - 1
+        ans = []
+        while l < r:
+            if arr2[l] + arr2[r] > target:
+                r -= 1
+            elif arr2[l] + arr2[r] < target:
+                l += 1
+            else:
+                if nums.index(arr2[l])!= nums.index(arr2[r]):
+                    ans.append(nums.index(arr2[l]))
+                    ans.append(nums.index(arr2[r]))
+                else:
+                    ans.append(nums.index(arr2[l]))
+                    ans.append(nums[l+1:].index(arr2[r])+l+1)
+                break
+        return ans
