d2a1586381228077539a972eba4f26fa02f41af6
Hiwot Derese <Hiwotderese16@gmail.com>
2022-10-15 23:15:57 +0300

    1423Leetcode

---
+++ Maximum Points You Can Obtain from.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxScore(self, cardPoints: List[int], k: int) -> int:
+        if k == len(cardPoints):
+            return sum(cardPoints)
+        maxm = sum(cardPoints[:k])
+        left = k - 1
+        right = len(cardPoints) - 1
+        Sum = maxm
+        # print(Sum)
+        while left >= 0:
+            Sum = Sum - cardPoints[left] + cardPoints[right]
+            # print(Sum)
+            maxm = max(maxm, Sum)
+            left -= 1
+            right -= 1
+        return maxm
+     
+
