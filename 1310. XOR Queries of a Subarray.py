5a24af020ef2126c2b0dbe95cfa44c6e6b0f7ce1
Hiwot Derese <Hiwotderese16@gmail.com>
2022-10-24 18:52:59 +0300

    Runtime 1064 ms Beats 10.79% Memory 29 MB Beats 16.79%

---
+++ 1310. XOR Queries of a Subarray.py
@@ -0,0 +1,16 @@
+class Solution:
+    def xorQueries(self, arr: List[int], queries: List[List[int]]) -> List[int]:
+        xorArr = [arr[0]]
+        ans = []
+        for i in range(1,len(arr)):
+            xorArr.append(xorArr[i - 1] ^ arr[i])
+        # print(xorArr)
+        for i in range(len(queries)):
+            # start = queries[i][0]
+            # end = queries[i][1]
+            if queries[i][0] == 0:
+                ans.append(xorArr[queries[i][1]])
+            else:
+                # print(start)
+                ans.append(xorArr[queries[i][1]] ^ xorArr[queries[i][0] - 1])
+        return ans
