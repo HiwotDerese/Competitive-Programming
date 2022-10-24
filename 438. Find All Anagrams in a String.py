53059e0302306271d72a13618aa327142ba138d3
Hiwot Derese <Hiwotderese16@gmail.com>
2022-10-24 07:07:21 +0300

    Runtime296msBeats53.26%Memor15.3MB

---
+++ 438. Find All Anagrams in a String.py
@@ -0,0 +1,28 @@
+class Solution:
+    def findAnagrams(self, s: str, p: str) -> List[int]:
+        if len(s) < len(p):
+            return []
+        hash1 = OrderedDict((i,0) for i in ascii_lowercase)
+        hash2 = OrderedDict((i,0) for i in ascii_lowercase)
+        ans = []
+        for i in range(len(p)):
+            hash1[p[i]] += 1
+            hash2[s[i]] += 1
+        i = 0
+        j = len(p)
+        while j < len(s):
+            if hash1 == hash2:
+                ans.append(i)
+            hash2[s[i]] -= 1
+            if j != len(s):
+                hash2[s[j]] += 1
+            i += 1
+            j += 1
+        if hash1 == hash2:
+            ans.append(i)
+        return ans
+            
+
+
+        
+        
