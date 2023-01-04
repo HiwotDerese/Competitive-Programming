d1c20713bec7c6642684b56f754acc73c74d02da
Hiwot Derese <Hiwotderese16@gmail.com>
2023-01-04 16:29:40 +0300

    comapre t_shirt

--- A_Compare_T-Shirt_Sizes.py
+++ A_Compare_T-Shirt_Sizes.py
@@ -3,5 +3,35 @@ test = int(input())
 # print(i)
 
 for i in range(test):
-    size = input().split()
+    tshirt_size = input().split()
+    t_size1 = tshirt_size[0]
+    t_size2 = tshirt_size[1]
+    leng1 = len(t_size1)
+    leng2 = len(t_size2)
+
+    # l < m < s
+    if t_size1[-1] < t_size2[-1]:
+        print(">")
+    elif t_size1[-1] > t_size2[-1]:
+        print("<")
+
+    # when both are the same ll,mm or ss
+    else:
+        if leng1 == leng2:
+            print("=")
+        elif t_size1[-1] == "S":
+            if leng1 < leng2:   
+                print(">")
+            elif leng1 > leng2:
+                print("<")
+        elif t_size1[-1] == "L":
+            if leng1 < leng2:   
+                print("<")
+            elif leng1 > leng2:
+                print(">")
+
+  
+    
+    
+    
     
---
+++ D_Pattern.py
@@ -0,0 +1,8 @@
+test = int(input())
+ 
+ans = []
+for i in range(test):
+    pattern = input().split()
+    for i in range(len(pattern)):
+        
+
