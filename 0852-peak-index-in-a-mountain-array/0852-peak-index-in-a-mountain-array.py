44688acbf635a0662dd17c5376d73b1fe4b26a73
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-03-07 08:15:24 +0300

    Time: 672 ms (16.82%), Space: 27.8 MB (28.70%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.py
@@ -8,8 +8,10 @@ class Solution:
             # print(low, high, mid)
             if arr[low] < arr[mid]:
                 low += 1
+                
             elif arr[low] == arr[mid] and arr[high] > arr[mid]:
                 low += 1
+                
             if arr[high] < arr[mid]:
                 high -= 1
             # print(low, high, mid)
