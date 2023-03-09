e64ff6438d9d59ecd40506f6662b3e131b900896
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-03-10 02:42:56 +0300

    Time: 52 ms (86.29%), Space: 18.6 MB (15.80%) - LeetHub

--- 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
+++ 0501-find-mode-in-binary-search-tree/0501-find-mode-in-binary-search-tree.py
@@ -5,30 +5,29 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def findMode(self, root: Optional[TreeNode]) -> List[int]:
-        
-        def mode(root, dic):
+    
+    def inorder(self, root, dic):
             
             if not root:
                 return
             
-            if root.val in dic:
-                dic[root.val] += 1
+            self.inorder(root.left, dic)
+            
+            dic[root.val] = dic.get(root.val, 0) + 1
                 
-            else:
-                dic[root.val] = 1
-                
-            mode(root.left, dic)
-            mode(root.right, dic)
-               
-    
+            self.inorder(root.right, dic)
+            
+            
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:     
         dic = {}
-        mode(root, dic)
+        
+        self.inorder(root, dic)
         
         sorted_dic = sorted(dic.items(), key=lambda x:x[1], reverse=True)
         most_freq = sorted_dic[0][1]
         
         ans, i = [], 0
+        
         while i < len(sorted_dic) and sorted_dic[i][1] == most_freq:
             ans.append(sorted_dic[i][0])
             i += 1
