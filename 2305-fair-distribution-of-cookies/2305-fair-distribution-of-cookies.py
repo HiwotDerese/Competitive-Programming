eb36f592fd29d1aaf2e1fcf7e70a565a5f35ce31
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-03-10 16:29:42 +0300

    Time: 2335 ms (24.18%), Space: 14 MB (28.57%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,40 +1,23 @@
 class Solution:
     
     def fairD(self, distribution, index, cookies, k):
-        
-        # print(distribution, index)
         if index == len(cookies):
+            
             return max(distribution)
         
         min_unfairness = float('inf')
+        
         for i in range(k):
             
             distribution[i] += cookies[index]
             
             if distribution[i] < min_unfairness:
                 min_unfairness = min(min_unfairness, self.fairD(distribution, index + 1, cookies, k))
-            
-            # print(distribution)
-            # self._max = max(self._max, )
-            # return self._max
-            # print(self._max, "max")
-            # self._min = min(self._min, self._max)
-            # print(self._min , "min")
-            
-            # print(i, distribution[i])
+
             distribution[i] -= cookies[index]
             
         return min_unfairness
-            
-            # return self._min
-            
 
-            # for j in range(1, k):
-            #     distribution[j] += cookies[i - 1]
-            #     self.fairD(distribution, index, cookies, k)
-                
-        # return _min
-    
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         
         index, self._max, self._min = 0, -float('inf'), float('inf')
