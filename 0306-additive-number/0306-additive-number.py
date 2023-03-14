7a9d6a95788fb2d6160c0994159a10112b0816e8
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-03-14 22:59:10 +0300

    Time: 31 ms (76.63%), Space: 13.8 MB (67.05%) - LeetHub

---
+++ 0306-additive-number/0306-additive-number.py
@@ -0,0 +1,50 @@
+# "112358"
+class Solution:
+    def additive(self, num, num1, num2, sum_, found):
+        if sum_ == "" and found:
+            return True
+        num3 = str(num1 + num2)
+        min_leng = min(len(num3), len(sum_))
+        if sum_[0: min_leng] == num3:
+            return self.additive(num, num2, int(num3), sum_[min_leng:], True)
+        return False
+        
+
+    def isAdditiveNumber(self, num: str) -> bool:
+
+        for i in range(1, len(num) - 1):
+            num1 = int(num[:i])
+
+#               if there are a leading zeros
+            if str(num1) != num[:i]:
+                break
+            for j in range(i + 1, len(num)):
+                num2 = int(num[i: j])
+                if str(num2) != num[i: j]:
+                    break
+                found = self.additive(num, num1, num2, num[j :], False)
+                if found:
+                    return True
+        print('ww')
+        return False
+                
+                
+        
+
+        
+            
+            
+            
+            
+            
+            
+            
+            
+            
+            
+
+        
+        
+        
+        
+        
