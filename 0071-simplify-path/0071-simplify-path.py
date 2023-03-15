b149f620d32b0182c73b7e3489279be08ef2373b
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-03-15 20:39:43 +0300

    Time: 37 ms (50.42%), Space: 14 MB (6.44%) - LeetHub

--- 0071-simplify-path/0071-simplify-path.py
+++ 0071-simplify-path/0071-simplify-path.py
@@ -5,39 +5,35 @@ class Solution:
         while idx < leng:
             if path[idx] == '/':
                 if not stack:
-                    # print(stack)
                     stack.append(path[idx])
-                    # print(stack)
+                    
                 elif curr:
                     if curr == ".." and len(stack) > 1:
-                        # print(stack)
                         stack.pop()
 
                     elif curr != ".." and curr != ".":
-                        # print(stack)
                         if stack[-1] == '/':
                             stack.append(curr)
+                            
                         else:
-                            # print(curr)
                             stack.append('/' + curr)
             
-                curr = ""    
+                curr = "" 
+                
             else:
                 curr += path[idx]
             
-            # print(stack)
             idx += 1
         
-        # print(curr)
         if curr:
-            # print(curr)
             if curr == ".." and len(stack) > 1:
                 stack.pop()
+                
             elif curr != ".." and curr != ".":
                 if stack[-1] == '/':
                     stack.append(curr)
+                    
                 else:
-                    # print(curr)
                     stack.append('/' + curr)
 
         return "".join(stack)
