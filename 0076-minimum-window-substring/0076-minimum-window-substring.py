5de3d8fe8570d8933c5cf997e2bd4d7750758520
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-03-18 14:46:50 +0300

    Time: 1496 ms (5.01%), Space: 14.7 MB (74.57%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,57 +1,22 @@
 class Solution:
     def minWindow(self, s: str, t: str) -> str:
-        tCounter = Counter(t) # counter for t to check with
-        window = Counter() # sliding window
-        ans = "" # answer
-        last = 0 # last index in our window
-        
-        for i,char in enumerate(s):
-            
-            window[char] = window.get(char,0)+1 # add this character to our window
-            
-            while window >= tCounter: # while we have all the necessary characters in our window
-                if ans == "" or i - last < len(ans): # if the answer is better than our last one
-                    ans = s[last:i+1] # update ans
+        t_dic = Counter(t) 
+        s_dic = Counter() 
+        ans, _min, left = "", float('inf'), 0
+
+        for right in range(len(s)):
+            s_dic[s[right]] = 1 + s_dic.get(s[right], 0)
+            # print(s_dic)
+            while s_dic >= t_dic: 
+                # print(s_dic)
+                if (right - left + 1) < _min:
+                    ans = s[left:right+1]
+                    _min = right - left + 1
                     
-                window[s[last]] -= 1 # remove the last element from our counter
-                last += 1 # move the last index forward
-                
-        return ans # return answer
-#     def minWindow(self, s: str, t: str) -> str:
-#         tCounter = Counter(t) 
-#         window = Counter() 
-#         ans = "" 
-#         last = 0 
+                s_dic[s[left]] -= 1
+                left += 1
 
-#         for i,char in enumerate(s):
-#             window[char] = window.get(char,0)
-
-#             while window >= tCounter: 
-#                 if ans == "" or i - last < len(ans):
-#                     ans = s[last:i+1] 
-
-#                 window[s[last]] -= 1
-#                 last += 1
-
-#         return ans 
-    #         dic1, dic2 = Counter(t), Counter()
-
-#         left, _min, ans = 0, float('inf'), ""
-        
-#         for right in range(len(s)):
-#             while dic1 <= dic2:
-#                 if right - left < _min:
-#                     ans = s[left:right]
-#                     _min = right - left
-
-#                 dic2[s[left]] -= 1
-#                 left += 1
-
-#             dic2[s[right]] = 1
-            
-#             right += 1
-            
-#         return ans
+        return ans 
                 
                 
         
