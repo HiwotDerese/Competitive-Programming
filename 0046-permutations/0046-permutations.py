e6101200933963a309d9268aced2a5a0fb8ed934
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-04-03 00:58:05 +0300

    Time: 49 ms (14.54%), Space: 14.1 MB (17.04%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -2,29 +2,28 @@ class Solution:
     
     def permutation(self, nums, index, permutations, i):
         leng = len(nums)
-        # print(permutations)
+        
         if permutations and len(permutations) == leng:
-            # print(permutations)
             self.ans.append(permutations.copy())
             return
         
         for j in range(leng):
-            # print(j, permutations)
             if i == j:
                 continue
                 
-            m = index & (1 << j) == 0
-            if m:
+            if index & (1 << j) == 0:
                 permutations.append(nums[j])
+                
                 self.permutation(nums, index + 2 ** j, permutations, i)
+                
                 permutations.pop()
                 
         
         
     def permute(self, nums: List[int]) -> List[List[int]]:
         self.ans, leng = [], len(nums)
+        
         for i in range(leng):
-            # print(i, self.ans)
             self.permutation(nums, 2 ** i, [nums[i]], i)
             
         return self.ans
