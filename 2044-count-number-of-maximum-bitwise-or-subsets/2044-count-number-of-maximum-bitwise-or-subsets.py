2863ed6b8233bc5371a6a8dc35f4962bafe695f9
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-04-03 22:46:45 +0300

    Time: 1853 ms (19.46%), Space: 14 MB (45.70%) - LeetHub

--- 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
+++ 2044-count-number-of-maximum-bitwise-or-subsets/2044-count-number-of-maximum-bitwise-or-subsets.py
@@ -8,25 +8,22 @@ class Solution:
             subset.append(nums[i])
             
             ans = reduce(lambda x, y: x | y, subset)
-            # print(subset, ans)
             
             dic[ans] = 1 + dic.get(ans, 0)
-            # print(subset, ans, dic, nums[i+1:])
             
             self.maxOr(nums[i+1:], subset, dic)
             subset.pop()
             
         return dic
             
-        
     def countMaxOrSubsets(self, nums: List[int]) -> int:
         
-        # ans = reduce(lambda x, y: x | y, nums)
         self.dic = {}
         
         self.maxOr(nums, [], self.dic)
         s = dict(sorted(self.dic.items(), key=lambda item: item[1]))
         idx = list(s)[-1]
+        
         return self.dic[idx]
     
     
