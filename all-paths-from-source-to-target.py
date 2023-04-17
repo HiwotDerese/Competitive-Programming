cc09ea41ee73f8f15b72dfbeb1b838d9324717d5
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-04-17 14:23:00 +0300

    added all-paths-from-source-to-target.python

--- all-paths-from-source-to-target.py
+++ all-paths-from-source-to-target.py
@@ -1,17 +1,13 @@
 class Solution:
     def paths(self,graph, path):
-        if path[-1] == self.leng - 1:
-            self.ans.append(path.copy())
+        if path[-1] == len(graph) - 1:
+            self.ans.append(path)
             return
 
         for i in graph[path[-1]]:
-            path.append(i)
-            self.paths(graph, path)
-            path.pop()
-
+            self.paths(graph, path + [i])
 
     def allPathsSourceTarget(self, graph: List[List[int]]) -> List[List[int]]:
         self.ans = []
-        self.leng = len(graph)
         self.paths(graph, [0])
         return self.ans
