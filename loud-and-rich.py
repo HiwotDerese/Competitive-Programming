8e136c4a4c07417694e31928381dea25fae1cf69
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-05-15 22:46:38 +0300

    added loud-and-rich.python

---
+++ loud-and-rich.py
@@ -0,0 +1,34 @@
+class Solution:
+
+    def dfs(self, num, minm, adj, quiet):
+        queue = deque([num])
+        visited = set([num])
+
+        while queue:
+            n = queue.popleft()
+            if quiet[n] < quiet[minm]:
+                minm = n
+
+            for i in adj[n]:
+                if i not in visited:
+                    visited.add(i)
+                    queue.append(i)
+
+        return minm
+
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        n, ans = len(quiet), []
+        adj = defaultdict(list)
+        
+        for i in richer:
+            adj[i[1]].append(i[0])
+
+        for i in range(n):
+            if i in adj:
+                min_ = self.dfs(i, i, adj, quiet)
+                ans.append(min_)
+
+            else:
+                ans.append(i)
+
+        return ans
