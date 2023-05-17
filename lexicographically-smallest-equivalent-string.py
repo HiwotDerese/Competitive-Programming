b1c3ae574e3a609b9f68e4e8d3fb2b4aefd21563
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-05-18 00:13:33 +0300

    added lexicographically-smallest-equivalent-string.python

---
+++ lexicographically-smallest-equivalent-string.py
@@ -0,0 +1,45 @@
+class Solution:
+
+    def find(self, x):
+        if self.parent[x] != x:
+            return self.find(self.parent[x])
+
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+
+        if root_x == root_y:
+            return
+
+        if self.rank[root_x ] > self.rank[root_y]:
+            self.parent[root_y] = root_x
+            self.minimum[root_x] = min(self.minimum[root_x], self.minimum[root_y])
+
+        elif self.rank[root_x] < self.rank[root_y]:
+            self.parent[root_x] = root_y
+            self.minimum[root_y] = min(self.minimum[root_y], self.minimum[root_x])
+
+        else:
+            self.parent[root_y] = root_x
+            self.minimum[root_x] = min(self.minimum[root_x], self.minimum[root_y])
+            self.rank[root_x] += 1
+
+    def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        self.parent = [i for i in range(26)]
+        self.rank = [0] * 26
+        self.minimum = [i for i in range(26)]
+
+        n = len(s1)
+        for i in range(n):
+            self.union(ord(s1[i]) - ord('a'), ord(s2[i]) - ord('a'))
+
+        m = len(baseStr)
+        ans = ""
+
+        for i in range(m):
+            root = self.find(ord(baseStr[i]) - ord('a'))
+            ans += chr(self.minimum[root] + ord('a'))
+
+        return ans
