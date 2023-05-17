cce1076f041df65ccbdc5203b2c990dc8c47dec0
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-05-17 17:45:25 +0300

    added find-if-path-exists-in-graph.python

---
+++ find-if-path-exists-in-graph.py
@@ -0,0 +1,35 @@
+class Solution:
+
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+            
+        return self.parent[x]
+
+    def union(self, x, y):
+        root_x = self.find(x)
+        root_y = self.find(y)
+        
+        if root_x != root_y:
+            if self.rank[root_x] < self.rank[root_y]:
+                self.parent[root_x] = root_y
+
+            elif self.rank[root_x] > self.rank[root_y]:
+                self.parent[root_y] = root_x
+
+            else:
+                self.parent[root_y] = root_x
+                self.rank[root_x] += 1
+
+    def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        self.parent = [i for i in range(n)]
+        self.rank = [0] * n
+        leng = len(edges)
+
+        for i in range(leng):
+            self.union(edges[i][0], edges[i][1])
+
+        source_p = self.find(source)
+        destination_p = self.find(destination)
+        2
+        return source_p == destination_p
