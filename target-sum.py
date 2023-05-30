c86eb1a7b668aa987a3b5d512ef9f9acaf0267d9
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-05-30 10:25:06 +0300

    added target-sum.python

--- target-sum.py
+++ target-sum.py
@@ -1,30 +1,21 @@
 class Solution:
-    def dfs(self, idx, sum_, n, nums, k):
+    def dfs(self, idx, sum_, nums, k):
 
         if (idx, sum_) in self.memo:
-            if self.memo[(idx, sum_)]:
-                self.ans += self.memo[(idx, sum_)]
-
             return self.memo[(idx, sum_)]
 
-        if idx == n:
-            if sum_ == k:
-                self.ans += 1
-                return 1
+        if idx == len(nums):
+            return sum_ == k
 
-            return 0
-
-        self.memo[(idx + 1, sum_ + nums[idx])] = self.dfs(idx + 1, sum_ + nums[idx], n, nums, k)
+        self.memo[(idx + 1, sum_ + nums[idx])] = self.dfs(idx + 1, sum_ + nums[idx], nums, k)
         
-        self.memo[(idx + 1, sum_ - nums[idx])] = self.dfs(idx + 1, sum_ - nums[idx], n, nums, k)
+        self.memo[(idx + 1, sum_ - nums[idx])] = self.dfs(idx + 1, sum_ - nums[idx], nums, k)
 
         self.memo[(idx, sum_)] = self.memo[(idx + 1, sum_ + nums[idx])]  + self.memo[(idx + 1, sum_ - nums[idx])]
 
         return self.memo[(idx, sum_)]
 
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        self.ans, self.memo = 0, {}
+        self.memo = {}
 
-        self.dfs(0, 0, len(nums), nums, target)
-        
-        return self.ans
+        return self.dfs(0, 0, nums, target)
