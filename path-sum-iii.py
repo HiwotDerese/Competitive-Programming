a2a9391243094390cdf0754841c95cc88bbc7155
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-08-22 15:40:37 +0300

    added path-sum-iii.python

---
+++ path-sum-iii.py
@@ -0,0 +1,33 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
+        self.ans = 0
+        dic = defaultdict(int)
+        dic[0] = 1
+        self.sum = 0
+
+        def backtrack(node):
+            if not node:
+                return
+            # print(node.val, self.sum)
+            self.sum += node.val
+            # print(node.val, self.sum)
+
+            if self.sum - targetSum  in dic:
+                self.ans += dic[self.sum - targetSum]
+
+            dic[self.sum] += 1
+    
+            left = backtrack(node.left)
+            right = backtrack(node.right)
+
+            dic[self.sum] -= 1
+            self.sum -= node.val
+
+        backtrack(root)
+        return self.ans
