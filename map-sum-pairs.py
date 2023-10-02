1b97afd0bece7e2721b6604ea00488e9bb510160
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-10-02 10:36:45 +0300

    added map-sum-pairs.python

--- map-sum-pairs.py
+++ map-sum-pairs.py
@@ -2,7 +2,6 @@ class TrieNode:
     def __init__(self):
         self.children = {}
         self.count = 0
-        # self.is_end_of_word = False
 
 class MapSum:
 
@@ -17,36 +16,25 @@ class MapSum:
             diff = val - self.dic_[key] 
             for char in key:
                 curr.children[char].count += diff
-                # curr.children[char] += diff
                 curr = curr.children[char]
 
         else:
-            # print(key)
             for char in key:
                 if char in curr.children:
-                    # print(char, curr.children)
                     curr.children[char].count += val
-                    # curr.children[char] += val
                 else:
                     curr.children[char] = TrieNode()
                     curr.children[char].count = val
-                    # curr.children[char] = val
-                # print(curr, self.root)
-                # print(curr.children, curr.count)
+
                 curr = curr.children[char]
 
         self.dic_[key] = val
-        # print(self.root, self.dic_, key)
 
     def sum(self, prefix: str) -> int:
         curr, ans, flag = self.root, 0, True
-        # print(curr.count, prefix)
 
         for char in prefix:
-            # print(curr.children, "children", char)
-            # print(char, prefix)
             if char not in curr.children:
-                # print(char, "wrrr")
                 flag = False
                 break
 
