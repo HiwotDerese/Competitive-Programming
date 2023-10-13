3a240961a777de787f99c517e82ad25b03c2f01e
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-10-14 00:49:22 +0300

    added reducing-dishes.python

--- reducing-dishes.py
+++ reducing-dishes.py
@@ -1,21 +1,18 @@
 class Solution:
     def maxSatisfaction(self, satisfaction: List[int]) -> int:
-        n, ans, sum_ = len(satisfaction), 0, 0
-        satisfaction.sort()
-        arr = []
-        start = 0
 
-        for i in range(n - 1, -1, -1):
-            if sum_ + satisfaction[i] >= 0:
-                arr.append(satisfaction[i])
-                sum_ += satisfaction[i]
-            
+        satisfaction.sort(reverse=True)  # Sort the satisfaction values in descending order.
+
+        n = len(satisfaction)
+        pre_sum, ans = 0, 0
+
+        for i in range(n):
+            pre_sum += satisfaction[i] 
+
+            if pre_sum >= 0:
+                ans += pre_sum
+
             else:
                 break
-
-        arr.reverse()
-        arr.insert(0, 0)
-        for i in range(len(arr)):
-            ans += (arr[i] * i)
 
         return ans
