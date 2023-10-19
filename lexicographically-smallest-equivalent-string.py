14f5f01e53d672f16a360b9f22a6e829342a8036
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-10-19 11:53:42 +0300

    added lexicographically-smallest-equivalent-string.python

--- lexicographically-smallest-equivalent-string.py
+++ lexicographically-smallest-equivalent-string.py
@@ -4,7 +4,7 @@ class Solution:
         if self.parent[x] != x:
             return self.find(self.parent[x])
 
-        return self.parent[x]
+        return x
 
     def union(self, x, y):
         root_x = self.find(x)
@@ -17,29 +17,28 @@ class Solution:
             self.parent[root_y] = root_x
             self.minimum[root_x] = min(self.minimum[root_x], self.minimum[root_y])
 
-        elif self.rank[root_x] < self.rank[root_y]:
-            self.parent[root_x] = root_y
-            self.minimum[root_y] = min(self.minimum[root_y], self.minimum[root_x])
-
         else:
+            if self.rank[root_x] == self.rank[root_y]:
+                self.rank[root_y] += 1
+
             self.parent[root_y] = root_x
             self.minimum[root_x] = min(self.minimum[root_x], self.minimum[root_y])
-            self.rank[root_x] += 1
+            
 
     def smallestEquivalentString(self, s1: str, s2: str, baseStr: str) -> str:
+        n, m = len(s1), len(baseStr)
+
         self.parent = [i for i in range(26)]
         self.rank = [0] * 26
         self.minimum = [i for i in range(26)]
 
-        n = len(s1)
-        for i in range(n):
-            self.union(ord(s1[i]) - 97, ord(s2[i]) - 97)
 
-        m = len(baseStr)
+        for idx in range(n):
+            self.union(ord(s1[idx]) - 97, ord(s2[idx]) - 97)
+
         ans = ""
-
         for i in range(m):
-            root = self.find(ord(baseStr[i]) - 97)
+            root = self.find(ord(baseStr[i]) - 97)            
             ans += chr(self.minimum[root] + 97)
 
         return ans
