3208b8d0cc066343edf7a5d895dab053c185a291
Hiwot Derese <91502861+HiwotDerese@users.noreply.github.com>
2023-10-19 11:45:41 +0300

    added unique-paths.python

--- unique-paths.py
+++ unique-paths.py
@@ -1,21 +1,13 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        dp = {}
+        dp = [[0 for row in range(n)] for col in range(m)]
 
-        def dfs(row, col):
+        for row in range(m):
+            for col in range(n):
+                if col == 0 and row == 0:
+                    dp[row][col] = 1
 
-            if (row, col) in dp:
-                return dp[(row, col)]
-                
-            if row == m or col == n:
-                return 0
+                else:
+                    dp[row][col] = dp[row][col - 1]  + dp[row - 1][col]
 
-            if (row, col) == (m - 1, n - 1):
-                return 1
-
-            dp[(row, col)] = dfs(row + 1, col) + dfs(row, col + 1)
-
-            return dp[(row, col)]
-
-        
-        return dfs(0, 0)
+        return dp[m - 1][n - 1]
